c4ecf7223de7a10bb6cad1e7ea164dae9062b985
alena <mittralena@yandex.ru>
2025-03-06 13:14:54 +0500

    add fields

--- src/components/BookModal.vue
+++ src/components/BookModal.vue
@@ -2,11 +2,12 @@
 const props = defineProps({
   data: {},
 });
-
-const { title, publishDate, authors, description } = props.data;
+const { title, publishedDate, authors, description } = props.data;
 </script>
 
 <template>
   <h1>{{ title }}</h1>
   <p>{{ description }}</p>
+  <p>Дата публикации: {{ publishedDate }}</p>
+  <p>Авторы: {{ authors }}</p>
 </template>
